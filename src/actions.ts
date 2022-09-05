7d06df08ab0768c3b8778b3759acd38266466b13
Marco Roth <marco.roth@intergga.ch>
2022-09-05 23:36:49 +0200

    Add tests for `clear_storage` action

--- src/actions.ts
+++ src/actions.ts
@@ -5,7 +5,7 @@ import * as Browser from "./actions/browser"
 import * as DOM from "./actions/dom"
 import * as Events from "./actions/events"
 import * as History from "./actions/history"
-import * as Storage from "./actions/browser"
+import * as Storage from "./actions/storage"
 
 export function register(streamActions: TurboStreamActions) {
   Attributes.register(streamActions)
---
+++ test/storage/clear_storage.test.js
@@ -0,0 +1,77 @@
+import { html, fixture, assert } from '@open-wc/testing'
+import { executeStream } from '../test_helpers'
+
+import * as Turbo from '@hotwired/turbo'
+
+import TurboPower from '../../'
+TurboPower.initialize(Turbo.StreamActions)
+
+describe('clear_storage', () => {
+  context('localStorage', () => {
+    it('should clear localStorage with not defined type', async () => {
+      localStorage.setItem('key1', 'value1')
+      localStorage.setItem('key2', 'value2')
+      sessionStorage.setItem('key3', 'value3')
+
+      assert.equal(localStorage.getItem('key1'), 'value1')
+      assert.equal(localStorage.getItem('key2'), 'value2')
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+
+      await executeStream('<turbo-stream action="clear_storage"></turbo-stream>')
+
+      assert.equal(localStorage.getItem('key1'), null)
+      assert.equal(localStorage.getItem('key2'), null)
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+    })
+
+    it('should clear localStorage with empty "type" attribute', async () => {
+      localStorage.setItem('key1', 'value1')
+      localStorage.setItem('key2', 'value2')
+      sessionStorage.setItem('key3', 'value3')
+
+      assert.equal(localStorage.getItem('key1'), 'value1')
+      assert.equal(localStorage.getItem('key2'), 'value2')
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+
+      await executeStream('<turbo-stream action="clear_storage" type=""></turbo-stream>')
+
+      assert.equal(localStorage.getItem('key1'), null)
+      assert.equal(localStorage.getItem('key2'), null)
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+    })
+
+    it('should clear localStorage with "type=local"', async () => {
+      localStorage.setItem('key1', 'value1')
+      localStorage.setItem('key2', 'value2')
+      sessionStorage.setItem('key3', 'value3')
+
+      assert.equal(localStorage.getItem('key1'), 'value1')
+      assert.equal(localStorage.getItem('key2'), 'value2')
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+
+      await executeStream('<turbo-stream action="clear_storage" type="local"></turbo-stream>')
+
+      assert.equal(localStorage.getItem('key1'), null)
+      assert.equal(localStorage.getItem('key2'), null)
+      assert.equal(sessionStorage.getItem('key3'), 'value3')
+    })
+  })
+
+  context('sessionStorage', () => {
+    it('should clear sessionStorage with "type=session"', async () => {
+      sessionStorage.setItem('key1', 'value1')
+      sessionStorage.setItem('key2', 'value2')
+      localStorage.setItem('key3', 'value3')
+
+      assert.equal(sessionStorage.getItem('key1'), 'value1')
+      assert.equal(sessionStorage.getItem('key2'), 'value2')
+      assert.equal(localStorage.getItem('key3'), 'value3')
+
+      await executeStream('<turbo-stream action="clear_storage" type="session"></turbo-stream>')
+
+      assert.equal(sessionStorage.getItem('key1'), null)
+      assert.equal(sessionStorage.getItem('key2'), null)
+      assert.equal(localStorage.getItem('key3'), 'value3')
+    })
+  })
+})
