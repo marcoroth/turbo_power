59536c78380e1cc691f752438cb45bb8b7d07173
Leon Vogt <nonick@nonick.ch>
2022-11-19 17:49:16 +0100

    Implement `turbo_clear_cache` action (#21)

---
+++ test/browser/turbo_clear_cache.test.js
@@ -0,0 +1,23 @@
+import { assert, oneEvent } from '@open-wc/testing'
+import { executeStream, registerAction, fixtureFile } from '../test_helpers'
+
+registerAction('turbo_clear_cache')
+
+describe('turbo_clear_cache', () => {
+  context('with history snapshots present', () => {
+    it('should clear the snapshots entries', async () => {
+      Turbo.visit(fixtureFile('page1.html'))
+      await oneEvent(document, 'turbo:load')
+
+      Turbo.visit(fixtureFile('page2.html'))
+      await oneEvent(document, 'turbo:load')
+      
+      Turbo.visit(fixtureFile('page3.html'))
+      await oneEvent(document, 'turbo:load')
+      
+      assert.isAtLeast((Object.keys(Turbo.session.view.snapshotCache.snapshots).length), 1)
+      await executeStream('<turbo-stream action="turbo_clear_cache"></turbo-stream>')
+      assert.isAtMost(Object.keys(Turbo.session.view.snapshotCache.snapshots).length, 0)
+    })
+  })
+})
