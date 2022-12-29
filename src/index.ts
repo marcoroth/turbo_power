c6e1ee4ec86280c6aeed5f76ba11bc207a393499
Marco Roth <marco.roth@intergga.ch>
2022-12-29 22:46:20 +0100

    Add `typecast` util function

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { TurboStreamAction, TurboStreamActions } from "@hotwired/turbo"
 import TurboReady from "turbo_ready"
 import * as TurboMorph from "turbo-morph"
 import * as Actions from "./actions"
+import * as Utils from "./utils"
+
+export * as Actions from "./actions"
+export * as Utils from "./utils"
 
 export function initialize(streamActions: TurboStreamActions) {
   TurboMorph.initialize(streamActions)
@@ -18,4 +22,5 @@ export default {
   initialize,
   register,
   Actions,
+  Utils
 }
