6da6b3fe7610db320464b4896fd1e94670fb6779
mario <github@lxxxvi.ch>
2023-01-23 21:11:56 +0100

    move set_cookie action from ./browser to ./document

--- src/actions/document.ts
+++ src/actions/document.ts
@@ -1,11 +1,18 @@
 import { StreamElement, TurboStreamActions } from "@hotwired/turbo"
 import { CookieStringBuilder } from "./document/cookie_string_builder"
 
+export function set_cookie(this: StreamElement) {
+  const cookie = this.getAttribute("cookie") || ""
+
+  document.cookie = cookie
+}
+
 export function set_cookie_item(this: StreamElement) {
   const cookieStringBuilder = new CookieStringBuilder(this)
   document.cookie = cookieStringBuilder.build()
 }
 
 export function registerDocumentActions(streamActions: TurboStreamActions) {
+  streamActions.set_cookie = set_cookie
   streamActions.set_cookie_item = set_cookie_item
 }
