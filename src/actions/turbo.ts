77483e596606d173f4ff61858e31de69ba40c9af
Marco Roth <marco.roth@intergga.ch>
2023-02-11 16:46:26 +0100

    Use `Turbo.visit()` by default in `redirect_to` action (#39)
    Resolves #13

--- src/actions/turbo.ts
+++ src/actions/turbo.ts
@@ -12,7 +12,7 @@ declare global {
 export function redirect_to(this: StreamElement) {
   const url = this.getAttribute("url") || "/"
   const turboAction = (this.getAttribute("turbo-action") || "advance") as Action
-  const turbo = this.getAttribute("turbo") === "true"
+  const turbo = this.getAttribute("turbo") !== "false"
 
   if (turbo && window.Turbo) {
     window.Turbo.visit(url, { action: turboAction })
