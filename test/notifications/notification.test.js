b0269d90062a0bf2d8a9db49dbf6446d4e0ac3c3
Marco Roth <marco.roth@intergga.ch>
2023-02-12 15:26:00 +0100

    Add test for `notification` action with escaped options

--- test/notifications/notification.test.js
+++ test/notifications/notification.test.js
@@ -116,5 +116,47 @@ describe("notification", () => {
         silent: true,
       })
     })
+
+    it("creates the notification with title and escaped options", async () => {
+      const stub = sinon.stub(window, "Notification")
+      sinon.stub(Notification, "permission").value("granted")
+
+      await executeStream(
+        `<turbo-stream
+          title="May I have your attention..."
+          dir="ltr"
+          lang="EN"
+          badge="https://example.com/badge.png"
+          body="This is displayed below the title."
+          tag="Demo" icon="https://example.com/icon.png"
+          image="https://example.com/image.png"
+          data="{&quot;arbitrary&quot;:&quot;data&quot;}"
+          vibrate="[200,100,200]"
+          renotify="true"
+          require-interaction="true"
+          actions="[{&quot;action&quot;:&quot;respond&quot;,&quot;title&quot;:&quot;Please respond&quot;,&quot;icon&quot;:&quot;https://example.com/icon.png&quot;}]"
+          silent="true"
+          action="notification"
+        ></turbo-stream>`
+      )
+
+      assert.equal(stub.callCount, 1)
+      assert.equal(stub.firstCall.firstArg, "May I have your attention...")
+      assert.deepEqual(stub.firstCall.args[1], {
+        dir: "ltr",
+        lang: "EN",
+        badge: "https://example.com/badge.png",
+        body: "This is displayed below the title.",
+        tag: "Demo",
+        icon: "https://example.com/icon.png",
+        image: "https://example.com/image.png",
+        data: { arbitrary: "data" },
+        vibrate: [200, 100, 200],
+        renotify: true,
+        requireInteraction: true,
+        actions: [{ action: "respond", title: "Please respond", icon: "https://example.com/icon.png" }],
+        silent: true,
+      })
+    })
   })
 })
