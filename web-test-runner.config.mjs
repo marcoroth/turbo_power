83c8fd4e530e2c9680237ef35438374acef610cb
Marco Roth <marco.roth@intergga.ch>
2023-09-15 18:30:43 +0200

    Filter browser logs in web test runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,19 @@
+const filteredLogs = [
+  'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.'
+]
+
+const filterBrowserLogs = (log) => {
+  for (const arg of log.args) {
+    if (typeof arg === 'string' && filteredLogs.some(l => arg.includes(l))) {
+      return false
+    }
+  }
+  return true
+}
+
 export default {
   nodeResolve: true,
+  filterBrowserLogs,
   mimeTypes: {},
   plugins: []
 }
